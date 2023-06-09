c3cfa4ebb1a9d8cd9d319a2708e2ca5b63f31f13
Terry <shiyuntian@126.com>
2023-06-08 20:13:46 -0400

    change url

--- pages/application.jsx
+++ pages/application.jsx
@@ -42,7 +42,7 @@ const netflix = () => {
             <button className='px-8 py-2 mt-4 mr-8'>Github Repo</button>
           </a>
           <a
-            href='https://fireclint.github.io/netflix-react-tailwind/'
+            href='https://application-tracking.vercel.app/'
             target='_blank'
             rel='noreferrer'
           >
